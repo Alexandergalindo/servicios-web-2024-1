d3dbb5c763c0b89b3c093e5c00f6dcccc19fdc66
alex <al.galindo25@ciaf.edu.co>
2024-03-20 19:04:40 -0500

    message

---
+++ operadores.js
@@ -0,0 +1,21 @@
+let counter = 100;
+console.log(counter)
+// suma
+counter = counter + 20;
+counter += 30;
+//resta
+counter = counter - 20;
+counter -= 30;
+//multiplicacion
+counter = counter * 3;
+counter *= 4;
+//division
+counter = counter /5;
+counter /= 4;
+//residuo
+counter = counter % 3;
+counter %= 3;
+console.log("linea 13" ,{counter});
+//exponencial
+counter = counter ** 3;
+counter **= 2;//
